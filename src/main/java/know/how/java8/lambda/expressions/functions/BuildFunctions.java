df6d4fec77c36b3a1aa9c285a3329c12f9bdb8cd
Peter Skurski <skurski@users.noreply.github.com>
2017-10-27 15:49:02 +0200

    Add files via upload

---
+++ src/main/java/know/how/java8/lambda/expressions/functions/BuildFunctions.java
@@ -0,0 +1,15 @@
+package know.how.java8.lambda.expressions.functions;
+
+/**
+ * Java 8 introduced reusable interfaces -> java.util.function package
+ * Those are functional interfaces that can be treated as functions
+ *
+ * TYPES (generic):
+ *      1. Predicate<T> - T in, boolean out
+ *      2. Function<T,R> - T in, R out
+ *      3. Consumer<T> - T in, void out
+ *      4. Supplier<R> - nothing in, R out
+ *      5. BinaryOperator<T> - Two T's in, T out
+ */
+public class BuildFunctions {
+}

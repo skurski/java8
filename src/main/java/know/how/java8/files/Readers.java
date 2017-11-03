0cd77ad1626aeb6a91b15a0f78efb8953d842bd9
Peter Skurski <skurski@users.noreply.github.com>
2017-11-03 09:17:30 +0100

    Add files via upload

--- src/main/java/know/how/java8/files/Readers.java
+++ src/main/java/know/how/java8/files/Readers.java
@@ -1,10 +1,14 @@
 package know.how.java8.files;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -19,16 +23,41 @@ public class Readers {
         System.out.println("Path for input file: " + path.toAbsolutePath());
 
         System.out.println("\nLine containing 'IT' check: ");
-        Files.lines(path).map(String::toUpperCase)
+        Files.lines(path)
+                .map(String::toUpperCase)
                 .filter(s -> s.contains("IT"))
                 .forEach(System.out::println);
 
         System.out.println("\nPalindrome check: ");
-        Files.lines(path)
+        List<String> palindromes = Files.lines(path)
                 .flatMap(line -> Stream.of(line.split(" ")))
                 .filter(word -> isPalindrome(word))
                 .map(String::toLowerCase)
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+        palindromes.forEach(System.out::println);
+
+        System.out.println("\nWrite palindrome to file: palindromes.txt");
+        Files.write(Paths.get("palindromes.txt"), palindromes, Charset.defaultCharset());
+
+        System.out.println("\nWrite something to file using more high level approach: output.txt");
+        try (PrintWriter writer = new PrintWriter(
+                Files.newBufferedWriter(Paths.get("output.txt"), Charset.defaultCharset()))) {
+
+            for (int i = 0; i < 10; i++) {
+                writer.printf("Number is %5.2f%n", 10 * Math.random());
+            }
+        }
+
+        System.out.println("\nList all files in root folder: ");
+        try (Stream<Path> paths = Files.walk(Paths.get("."))) {
+            paths.forEach(System.out::println);
+        }
+
+        System.out.println("\nList all text files in root folder: ");
+        try (Stream<Path> paths = Files.list(Paths.get("."))) {
+            paths.filter(p -> p.toString().endsWith(".txt"))
+                    .forEach(System.out::println);
+        }
 
         System.out.println("\nFibonacci 15 first numbers: ");
         System.out.println(Arrays.toString(fibonacci(15)));
